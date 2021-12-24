7f1c8fe990d724df1c2de04221302547a16f05d1
wyoshi <wyoshi@users.noreply.github.com>
2021-12-24 00:42:42 +0000

    deploy: 7f29e3d5bb93be74644ace823fe467b04808d395

---
+++ js/Custom.js
@@ -0,0 +1,25 @@
+class Custom {
+  constructor() {
+    this.ui = {
+      //アーカイブの選択
+      selectArchives: '#select-archives'
+    };
+  }
+  //イベントを登録
+  addEvents() {
+    let evChangeArchives = () => {
+      document.querySelector(this.ui.selectArchives).addEventListener('change', e => {
+        let val = e.currentTarget.value;
+        location.href = val;
+      }, false);
+    };
+    evChangeArchives();
+  }
+  //実行
+  init(){
+    this.addEvents();
+  }
+}
+
+let custom = new Custom();
+custom.init();
